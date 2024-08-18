51c63fb23bfdb84222ccdb8ea20d2ed16086dc18
k1ju <gksrlwn98@naver.com>
2024-08-18 13:49:27 +0900

    Fix: 비밀번호 형식 유효성검사 수정

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+import { IsEmail, Length, Matches } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({
@@ -10,8 +10,14 @@ export class CreateUserDto {
   @Length(2, 20)
   email: string;
 
-  @ApiProperty({ example: 'pw123', description: '2-10자, 비밀번호' })
-  @IsNotEmpty()
-  @Length(2, 10)
+  @ApiProperty({
+    example: 'pw123!!',
+    description: '6-30자의 비밀번호, 문자, 숫자, 특수문자 포함',
+  })
+  @Matches(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+=-]).{6,30}$/, {
+    message:
+      '비밀번호는 6-30자로 구성되며, 문자, 숫자, 특수문자를 반드시 포함해야합니다.',
+  })
+  @Length(6, 30)
   pw: string;
 }

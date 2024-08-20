13c505e5660b9d5dc0e95ba8c8893f97fa22ec48
k1ju <gksrlwn98@naver.com>
2024-08-20 21:44:47 +0900

    Fix: 프리즈마 스키마 계정 뷰 추가

---
+++ prisma/views/public/AccountDetailView.sql
@@ -0,0 +1,32 @@
+SELECT
+  a.idx,
+  a.email,
+  a.nickname,
+  a.profile,
+  a.provider,
+  a.created_at,
+  a.deleted_at,
+  a.serial_number,
+  p.img_path AS profile_img,
+  count(f1.follower_idx) AS follower_count,
+  count(f2.followee_idx) AS followee_count,
+  count(r.reported_idx) AS account_reported_count
+FROM
+  (
+    (
+      (
+        (
+          account_tb a
+          LEFT JOIN profile_img_tb p ON (((a.idx) :: text = (p.account_idx) :: text))
+        )
+        LEFT JOIN follow_tb f1 ON (((a.idx) :: text = (f1.follower_idx) :: text))
+      )
+      LEFT JOIN follow_tb f2 ON (((a.idx) :: text = (f2.followee_idx) :: text))
+    )
+    LEFT JOIN account_report_tb r ON (((a.idx) :: text = (r.reported_idx) :: text))
+  )
+GROUP BY
+  a.idx,
+  a.email,
+  a.nickname,
+  p.img_path;
---
+++ prisma/views/public/AccountInfoView.sql
@@ -0,0 +1,11 @@
+SELECT
+  account_tb.email,
+  account_tb.nickname,
+  account_tb.profile,
+  account_tb.provider,
+  account_tb.created_at,
+  account_tb.deleted_at,
+  account_tb.idx,
+  account_tb.serial_number
+FROM
+  account_tb;

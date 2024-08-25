c4df583ad5237ac9fbf8c70ce68fe565d9649885
k1ju <gksrlwn98@naver.com>
2024-08-25 13:06:57 +0900

    Fix: 리뷰 검색, 리뷰수정, 리뷰 생성 서비스 유저 엔티티도 반환

--- prisma/views/public/AccountInfoView.sql
+++ prisma/views/public/AccountInfoView.sql
@@ -6,6 +6,10 @@ SELECT
   account_tb.created_at,
   account_tb.deleted_at,
   account_tb.idx,
-  account_tb.serial_number
+  account_tb.serial_number,
+  p.img_path
 FROM
-  account_tb;
+  (
+    account_tb
+    LEFT JOIN profile_img_tb p ON (((account_tb.idx) :: text = (p.account_idx) :: text))
+  );

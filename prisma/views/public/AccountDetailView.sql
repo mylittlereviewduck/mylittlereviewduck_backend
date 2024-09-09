86f0d8a60d147e941d17c278f8b23215d1389d2b
k1ju <gksrlwn98@naver.com>
2024-09-09 13:04:59 +0900

    Fix: remove account_report_tb, review_report_tb, add report_tb

--- prisma/views/public/AccountDetailView.sql
+++ prisma/views/public/AccountDetailView.sql
@@ -8,24 +8,20 @@ SELECT
   a.deleted_at,
   a.serial_number,
   p.img_path AS profile_img,
-  (count(f1.follower_idx)) :: integer AS follower_count,
-  (count(f2.followee_idx)) :: integer AS followee_count,
-  (count(r.reported_idx)) :: integer AS account_reported_count,
+  (count(DISTINCT f1.follower_idx)) :: integer AS follower_count,
+  (count(DISTINCT f2.followee_idx)) :: integer AS followee_count,
   a.interest_1,
   a.interest_2
 FROM
   (
     (
       (
-        (
-          account_tb a
-          LEFT JOIN profile_img_tb p ON (((a.idx) :: text = (p.account_idx) :: text))
-        )
-        LEFT JOIN follow_tb f1 ON (((a.idx) :: text = (f1.follower_idx) :: text))
+        account_tb a
+        LEFT JOIN profile_img_tb p ON (((a.idx) :: text = (p.account_idx) :: text))
       )
-      LEFT JOIN follow_tb f2 ON (((a.idx) :: text = (f2.followee_idx) :: text))
+      LEFT JOIN follow_tb f1 ON (((a.idx) :: text = (f1.follower_idx) :: text))
     )
-    LEFT JOIN account_report_tb r ON (((a.idx) :: text = (r.reported_idx) :: text))
+    LEFT JOIN follow_tb f2 ON (((a.idx) :: text = (f2.followee_idx) :: text))
   )
 GROUP BY
   a.idx,

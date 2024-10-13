e15a1fa01c409b9aeaa49783a540a0bf29f2aa59
k1ju <gksrlwn98@naver.com>
2024-10-13 16:13:57 +0900

    fix: fix file name

---
+++ src/email/email.module.ts
@@ -0,0 +1,35 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { Module } from '@nestjs/common';
+import { EmailService } from '../email/Email.service';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+@Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        port: 587,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
+        },
+      },
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+      preview: true,
+      template: {
+        dir: process.cwd() + '/template/',
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+    PrismaModule,
+  ],
+  providers: [EmailService],
+  exports: [EmailService],
+})
+export class EmailModule {}

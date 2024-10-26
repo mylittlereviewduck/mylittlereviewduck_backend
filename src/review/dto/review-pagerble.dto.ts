0683b0fa891af8df9de59b6186910f49cb234b71
k1ju <gksrlwn98@naver.com>
2024-10-26 22:05:10 +0900

    Fix: fix ReviewPagerbleDto swagger

--- src/review/dto/review-pagerble.dto.ts
+++ src/review/dto/review-pagerble.dto.ts
@@ -5,7 +5,7 @@ import { Transform } from 'class-transformer';
 
 export class ReviewPagerbleDto {
   @ApiProperty({
-    description: '한 페이지에 담긴 리뷰 수 (기본값: 10)',
+    description: '한 페이지에 담긴 리뷰 수',
     default: 10,
   })
   @IsOptional()
@@ -14,7 +14,7 @@ export class ReviewPagerbleDto {
   size: number = 10;
 
   @ApiProperty({
-    description: '가져올 페이지 (기본값: 1)',
+    description: '가져올 페이지',
     default: 1,
   })
   @IsOptional()
@@ -26,7 +26,7 @@ export class ReviewPagerbleDto {
 
   @ApiProperty({
     description:
-      '검색기간: "1D" or "7D" or "1M" or 1Y" or all 로 주어져야함(기본값 all)',
+      '검색기간: "1D" or "7D" or "1M" or 1Y" or all 로 주어져야합니다.',
     default: 'all',
   })
   @IsIn(['1D', '7D', '1M', '1Y', 'all'])

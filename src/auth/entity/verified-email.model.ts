9aec93cf5e2cda4d9d1d02d6fa9c56f6462a6f93
k1ju <gksrlwn98@naver.com>
2025-01-26 16:40:58 +0900

    Feat: EmailVerificationEntity

--- src/auth/entity/verified-email.model.ts
+++ src/auth/entity/verified-email.model.ts
@@ -1,6 +1,22 @@
+import { Prisma } from '@prisma/client';
+
+const emailVerification =
+  Prisma.validator<Prisma.EmailVerificaitonTbAggregateArgs>()({});
+
+export type EmailVerificaiton = Prisma.EmailVerificaitonTbGetPayload<
+  typeof emailVerification
+>;
+
 export class EmailVerificationEntity {
   email: string;
   code: number;
   createdAt: Date;
   verifiedAt: Date;
+
+  constructor(data: EmailVerificaiton) {
+    this.code = data.code;
+    this.email = data.email;
+    this.createdAt = data.createdAt;
+    this.verifiedAt = data.verifiedAt;
+  }
 }

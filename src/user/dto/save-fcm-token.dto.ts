c630ccc610f2888c98d873d9adb3bc6fadd014d9
k1ju <gksrlwn98@naver.com>
2025-02-08 17:53:51 +0900

    Fix: createFcmTokenDto

--- src/user/dto/save-fcm-token.dto.ts
+++ src/user/dto/save-fcm-token.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
 export class CreateFcmTokenDto {
-  userIdx: string;
+  @ApiProperty({ description: 'fcm토큰' })
+  @IsString()
   token: string;
+
+  @ApiProperty({ description: '디바이스 식별자' })
+  @IsString()
   deviceIdx: string;
 }

aef3fde1037cb88ca990b59fc0bde79406dbf663
k1ju <gksrlwn98@naver.com>
2025-02-08 15:05:19 +0900

    Fix: main

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,6 @@ async function bootstrap() {
     토큰이 유효하지않을 경우 상태코드 401, "message": "Invalid Token"으로 반환됩니다. \n
     토큰이 없을때는 상태코드 401, "message": "No Token"으로 반환됩니다. \n 
     관리자 권한이 없을때는 상태코드 403, "message": "No Admin"으로 반환됩니다 \n
-    리소스가없는경우 404
     `,
     )
     .build();

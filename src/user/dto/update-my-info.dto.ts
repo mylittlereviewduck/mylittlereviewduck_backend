ea403d1073fd400692ef2f796c3e01dccd628c44
k1ju <gksrlwn98@naver.com>
2025-02-10 22:13:39 +0900

    Fix: updateMyInfoDto

--- src/user/dto/update-my-info.dto.ts
+++ src/user/dto/update-my-info.dto.ts
@@ -2,19 +2,19 @@ import { ApiProperty } from '@nestjs/swagger';
 import { ArrayMaxSize, IsOptional, IsString, Length } from 'class-validator';
 
 export class UpdateMyInfoDto {
-  @ApiProperty({ description: '2-16자 닉네임' })
+  @ApiProperty({ description: '2-16자 닉네임', required: false })
   @IsString()
   @Length(2, 16)
   @IsOptional()
   nickname?: string;
 
-  @ApiProperty({ description: '0-200자 프로필' })
+  @ApiProperty({ description: '0-200자 프로필', required: false })
   @IsString()
   @Length(0, 200)
   @IsOptional()
   profile?: string;
 
-  @ApiProperty({ description: '유저 관심사' })
+  @ApiProperty({ description: '유저 관심사', required: false })
   @IsString({ each: true })
   @ArrayMaxSize(2)
   @Length(0, 16, { each: true })

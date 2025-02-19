c1dead0d63394157fc1b8a41b7c92742bfeb2332
k1ju <gksrlwn98@naver.com>
2025-02-20 07:32:40 +0900

    Fix: remove checkDuplicatedResponseDto

--- src/user/dto/response/check-email-duplicate-response.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CheckEmailDuplicateReponseDto {
-  @ApiProperty({ description: '중복여부 true/false로 반환' })
-  isDuplicated: boolean;
-}
--- src/user/dto/response/check-nickname-duplicate-response.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean } from 'class-validator';
-
-export class CheckNicknameDuplicateResponseDto {
-  @ApiProperty({ description: '중복여부 true/false로 반환' })
-  @IsBoolean()
-  isDuplicated: boolean;
-}

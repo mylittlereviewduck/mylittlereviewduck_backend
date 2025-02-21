685f413780ed278cf4871ceb5ea4bbcd2b47ac6d
k1ju <gksrlwn98@naver.com>
2025-02-21 23:54:32 +0900

    Fix: remove console

--- src/common/fileValidation.pipe.ts
+++ src/common/fileValidation.pipe.ts
@@ -6,7 +6,6 @@ export class FileValidationPipe implements PipeTransform {
   private readonly maxSize = 10 * 1024 * 1024;
 
   transform(file: Express.Multer.File) {
-    console.log('file: ', file);
     if (!file) {
       throw new BadRequestException('No file');
     }

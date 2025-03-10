9fbe91bebbf12edd17956af9c7fe263a5e8af8f9
k1ju <gksrlwn98@naver.com>
2025-03-10 17:23:48 +0900

    Feat: add updateMyOpinion

--- src/user/dto/upsert-opinion.dto.ts
+++
@@ -1,5 +0,0 @@
-export class upsertOpinionDto {
-  userIdx: string;
-  title: string;
-  content: string;
-}
--- src/user/opinion.service.ts
+++ src/user/opinion.service.ts
@@ -1,19 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { OpinionEntity } from './entity/Opinion.entity';
-import { upsertOpinionDto } from './dto/upsert-opinion.dto';
+import { createOpinionDto } from './dto/create-opinion.dto';
+import { updateOpinionDto } from './dto/update-opinion.dto';
 
 @Injectable()
 export class OpinionService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async createOpinion(dto: upsertOpinionDto): Promise<OpinionEntity> {
+  async createOpinion(
+    userIdx: string,
+    dto: createOpinionDto,
+  ): Promise<OpinionEntity> {
     const newOpinionData = await this.prismaService.opinionTb.create({
       include: {
         accountTb: true,
       },
       data: {
-        accountIdx: dto.userIdx,
+        accountIdx: userIdx,
         title: dto.title,
         content: dto.content,
       },
@@ -33,6 +41,33 @@ export class OpinionService {
     });
 
     if (!opinionData) return null;
+
+    return new OpinionEntity(opinionData);
+  }
+
+  async updateMyOpinion(
+    userIdx: string,
+    dto: updateOpinionDto,
+  ): Promise<OpinionEntity> {
+    const myOpinion = await this.getOpinion(dto.idx);
+
+    if (!myOpinion) throw new NotFoundException('Not Found Opinion');
+
+    if (userIdx !== myOpinion.user.idx)
+      throw new UnauthorizedException('Unauthorized');
+
+    const opinionData = await this.prismaService.opinionTb.update({
+      where: {
+        idx: dto.idx,
+      },
+      data: {
+        title: dto.title,
+        content: dto.content,
+      },
+      include: {
+        accountTb: true,
+      },
+    });
 
     return new OpinionEntity(opinionData);
   }

66874872100c7c72bc581715b521e3dbe66ac319
k1ju <gksrlwn98@naver.com>
2025-03-10 17:37:13 +0900

    Fix: userController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -56,7 +56,6 @@ import { FcmTokenService } from './fcm-token.service';
 import { SearchHistoryResponseDto } from './dto/response/search-history.dto';
 import { SearchKeywordService } from './search-keyword.service';
 import { GetUserSearchDto } from './dto/get-users-search.dto';
-import { HotKeyword } from './dto/hot-keyword.type';
 import { PagerbleDto } from './dto/pagerble.dto';
 import { HotKeywordResponseDto } from './dto/response/hot-keyword-response.dto';
 
@@ -75,223 +74,29 @@ export class UserController {
     private readonly searchKeywordService: SearchKeywordService,
   ) {}
 
-  @Post('/check-nickname')
+  @Post('/:userIdx/follow')
+  @UseGuards(AuthGuard)
   @HttpCode(200)
-  @ApiOperation({ summary: '닉네임 중복검사' })
-  @Exception(400, '유효하지 않은 요청')
-  @Exception(409, '중복된 닉네임')
-  @ApiResponse({
-    status: 200,
-    description: '사용가능한 닉네임일경우 상태코드 200반환',
-  })
-  async checkNicknameDuplicate(
-    @Body() dto: CheckNicknameDuplicateDto,
-  ): Promise<void> {
-    const duplicatedUser = await this.userService.getUser({
-      nickname: dto.nickname,
-    });
-
-    if (duplicatedUser && duplicatedUser.nickname == dto.nickname) {
-      throw new ConflictException('Duplicated Nickname');
-    }
-
-    if (dto.nickname && dto.nickname.includes('번째 오리')) {
-      throw new BadRequestException("Nickname can't include '번째 오리'");
-    }
-  }
-
-  @Post('/signup')
-  @ApiOperation({ summary: '회원가입' })
-  @Exception(400, '유효하지 않은 요청')
-  @Exception(409, '유효하지않은 닉네임/이메일이거나 이미가입된 회원입니다')
-  @ApiResponse({ status: 201, type: UserEntity })
-  async signUp(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
-    return await this.userService.createUser(createUserDto);
-  }
-
-  @Post('pw/reset')
-  @ApiOperation({
-    summary: '비밀번호 변경',
-    description: '비밀번호 변경 성공시 상태코드 200반환',
-  })
-  @HttpCode(200)
-  @Exception(400, '유효하지 않은 요청')
-  @Exception(401, '권한 없음')
-  @ApiResponse({ status: 200 })
-  async resetPassword(@Body() dto: ResetPasswordDto): Promise<void> {
-    await this.userService.updatePassword(dto.email, dto.pw);
-  }
-
-  @Get('myinfo')
-  @UseGuards(AuthGuard)
-  @ApiOperation({ summary: '내정보보기' })
+  @ApiOperation({ summary: '유저 팔로우' })
   @ApiBearerAuth()
-  @Exception(401, '권한 없음')
-  @ApiResponse({ status: 200, type: UserEntity })
-  async GetMyInfo(@GetUser() loginUser: LoginUser): Promise<UserEntity> {
-    return await this.userService.getUser({ idx: loginUser.idx });
-  }
-
-  @Put('myinfo')
-  @UseGuards(AuthGuard)
-  @ApiOperation({
-    summary: '내정보수정',
-    description: '최소 1개이상의 속성을 주셔야합니다. ',
-  })
-  @ApiBearerAuth()
-  @Exception(400, '유효하지 않은 요청')
-  @Exception(401, '권한 없음')
-  @ApiResponse({ status: 200 })
-  async updateMyInfo(
-    @GetUser() loginUser: LoginUser,
-    @Body() dto: UpdateMyInfoDto,
-  ): Promise<UserEntity> {
-    return await this.userService.updateMyinfo(loginUser.idx, dto);
-  }
-
-  @Put('profile-img')
-  @UseGuards(AuthGuard)
-  @ApiBearerAuth()
-  @UseInterceptors(FileInterceptor('image'))
-  @ApiOperation({ summary: '프로필 이미지 수정' })
-  @ApiConsumes('multipart/form-data')
-  @ApiBody({
-    required: true,
-    description: '프로필 이미지',
-    schema: {
-      type: 'object',
-      properties: {
-        imgPath: {
-          type: 'string',
-          format: 'binary',
-          description: '업로드할 이미지 파일',
-        },
-      },
-    },
-  })
-  @Exception(400, '유효하지 않은 요청')
-  @Exception(401, '권한 없음')
-  @ApiResponse({ status: 200 })
-  async updateMyProfileImg(
-    @GetUser() loginUser: LoginUser,
-    @UploadedFile(FileValidationPipe) image: Express.Multer.File,
-  ): Promise<{ imgPath: string }> {
-    const imgPath = await this.awsService.uploadImageToS3(image);
-
-    await this.userService.updateMyProfileImg(loginUser.idx, imgPath, null);
-
-    return { imgPath: imgPath };
-  }
-
-  @Delete('profile-img')
-  @UseGuards(AuthGuard)
-  @ApiOperation({ summary: '프로필 이미지 삭제' })
-  @ApiBearerAuth()
-  @Exception(401, '권한 없음')
-  @ApiResponse({ status: 200 })
-  async deleteMyProfileImg(@GetUser() loginUser: LoginUser): Promise<void> {
-    await this.userService.updateMyProfileImg(loginUser.idx, null, null);
-  }
-
-  @Get('/:userIdx/info')
-  @UseGuards(OptionalAuthGuard)
-  @ApiOperation({ summary: '유저 정보 보기' })
   @ApiParam({
     name: 'userIdx',
     description: '유저idx',
     type: 'string',
-    example: '344e753e-9071-47b2-b651-bc32a0a92b1f',
+    example: '836d533b-3ee3-4616-8644-a1ddea65e1e0',
   })
   @Exception(400, '유효하지 않은 요청')
-  @ApiResponse({ status: 200, type: UserEntity })
-  async getUserInfo(
-    @Param('userIdx', ParseUUIDPipe) userIdx: string,
-    @GetUser() loginUser: LoginUser,
-  ): Promise<UserEntity> {
-    const user = await this.userService.getUser({
-      idx: userIdx,
-    });
-
-    if (!user) {
-      throw new NotFoundException('Not Found User');
-    }
-
-    if (!loginUser) {
-      return user;
-    }
-
-    await this.userFollowService.isFollow(loginUser.idx, [user]);
-
-    await this.userBlockCheckService.isBlockedUser(loginUser.idx, [user]);
-
-    return user;
-  }
-
-  @Get('')
-  @UseGuards(OptionalAuthGuard)
-  @ApiOperation({ summary: '유저검색하기 이메일, 닉네임, 관심사' })
-  @ApiQuery({ name: 'search', description: '검색 키워드, 검색어 2글자 이상' })
-  @Exception(400, '유효하지 않은 요청')
-  @Exception(404, 'Not Found Page')
-  @ApiResponse({ status: 200, type: UserPagerbleResponseDto })
-  async getUsersWithSearch(
-    @GetUser() loginUser: LoginUser,
-    @Query() dto: GetUserSearchDto,
-  ): Promise<UserPagerbleResponseDto> {
-    return await this.userService.getSearchedUsersWithInteraction(
-      dto,
-      loginUser,
-    );
-  }
-
-  @Get('search/hot')
-  @ApiOperation({
-    summary: '인기 검색어 보기',
-    description: '12시간마다 집계되어 최대 10개씩 반환됩니다.',
-  })
+  @Exception(401, '권한 없음')
   @ApiResponse({
     status: 200,
-    type: HotKeywordResponseDto,
+    description: '팔로우 성공 200 반환',
+    type: FollowEntity,
   })
-  async getHotSearchKeyword(): Promise<HotKeywordResponseDto> {
-    return {
-      keywords: await this.searchKeywordService.getCachedHotSearchKeywod(),
-    };
-  }
-
-  @Get('/search/history')
-  @UseGuards(AuthGuard)
-  @ApiBearerAuth()
-  @ApiOperation({
-    summary: '유저 검색기록 불러오기',
-    description: '최신순으로 검색 키워드 최대 10개까지 불러옵니다.',
-    deprecated: true,
-  })
-  @Exception(400, '유효하지 않은 요청')
-  @ApiResponse({ status: 200, type: SearchHistoryResponseDto })
-  async getMySearchHistory(
+  async followUser(
     @GetUser() loginUser: LoginUser,
-  ): Promise<SearchHistoryResponseDto> {
-    return await this.searchKeywordService.getUserSearchHistory(loginUser.idx);
-  }
-
-  @Post('/fcm/token')
-  @ApiOperation({ summary: 'fcm토큰 저장하기' })
-  @HttpCode(200)
-  @Exception(400, 'bad request')
-  @ApiResponse({ status: 200 })
-  async saveUserFcmToken(@Body() dto: CreateFcmTokenDto): Promise<void> {
-    await this.fcmTokenService.saveFcmToken(dto.fcmToken, dto.deviceIdx);
-  }
-
-  @Delete('')
-  @UseGuards(AuthGuard)
-  @ApiOperation({ summary: '유저 탈퇴하기' })
-  @ApiBearerAuth()
-  @Exception(401, '권한 없음')
-  @ApiResponse({ status: 200 })
-  async deleteUser(@GetUser() loginUser: LoginUser): Promise<void> {
-    await this.userService.deleteUser(loginUser.idx);
+    @Param('userIdx', ParseUUIDPipe) userIdx: string,
+  ): Promise<FollowEntity> {
+    return await this.followService.followUser(loginUser.idx, userIdx);
   }
 
   @Get('/:userIdx/following/all')
@@ -368,31 +173,6 @@ export class UserController {
     }
 
     return userPagerbleResponseDto;
-  }
-
-  @Post('/:userIdx/follow')
-  @UseGuards(AuthGuard)
-  @HttpCode(200)
-  @ApiOperation({ summary: '유저 팔로우' })
-  @ApiBearerAuth()
-  @ApiParam({
-    name: 'userIdx',
-    description: '유저idx',
-    type: 'string',
-    example: '836d533b-3ee3-4616-8644-a1ddea65e1e0',
-  })
-  @Exception(400, '유효하지 않은 요청')
-  @Exception(401, '권한 없음')
-  @ApiResponse({
-    status: 200,
-    description: '팔로우 성공 200 반환',
-    type: FollowEntity,
-  })
-  async followUser(
-    @GetUser() loginUser: LoginUser,
-    @Param('userIdx', ParseUUIDPipe) userIdx: string,
-  ): Promise<FollowEntity> {
-    return await this.followService.followUser(loginUser.idx, userIdx);
   }
 
   @Delete('/:userIdx/follow')
@@ -543,5 +323,224 @@ export class UserController {
       page: dto.page || 1,
       size: dto.size || 10,
     });
+  }
+
+  @Get('/:userIdx/info')
+  @UseGuards(OptionalAuthGuard)
+  @ApiOperation({ summary: '유저 정보 보기' })
+  @ApiParam({
+    name: 'userIdx',
+    description: '유저idx',
+    type: 'string',
+    example: '344e753e-9071-47b2-b651-bc32a0a92b1f',
+  })
+  @Exception(400, '유효하지 않은 요청')
+  @ApiResponse({ status: 200, type: UserEntity })
+  async getUserInfo(
+    @Param('userIdx', ParseUUIDPipe) userIdx: string,
+    @GetUser() loginUser: LoginUser,
+  ): Promise<UserEntity> {
+    const user = await this.userService.getUser({
+      idx: userIdx,
+    });
+
+    if (!user) {
+      throw new NotFoundException('Not Found User');
+    }
+
+    if (!loginUser) {
+      return user;
+    }
+
+    await this.userFollowService.isFollow(loginUser.idx, [user]);
+
+    await this.userBlockCheckService.isBlockedUser(loginUser.idx, [user]);
+
+    return user;
+  }
+
+  @Get('search/hot')
+  @ApiOperation({
+    summary: '인기 검색어 보기',
+    description: '12시간마다 집계되어 최대 10개씩 반환됩니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    type: HotKeywordResponseDto,
+  })
+  async getHotSearchKeyword(): Promise<HotKeywordResponseDto> {
+    return {
+      keywords: await this.searchKeywordService.getCachedHotSearchKeywod(),
+    };
+  }
+
+  @Get('/search/history')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: '유저 검색기록 불러오기',
+    description: '최신순으로 검색 키워드 최대 10개까지 불러옵니다.',
+    deprecated: true,
+  })
+  @Exception(400, '유효하지 않은 요청')
+  @ApiResponse({ status: 200, type: SearchHistoryResponseDto })
+  async getMySearchHistory(
+    @GetUser() loginUser: LoginUser,
+  ): Promise<SearchHistoryResponseDto> {
+    return await this.searchKeywordService.getUserSearchHistory(loginUser.idx);
+  }
+
+  @Post('/fcm/token')
+  @ApiOperation({ summary: 'fcm토큰 저장하기' })
+  @HttpCode(200)
+  @Exception(400, 'bad request')
+  @ApiResponse({ status: 200 })
+  async saveUserFcmToken(@Body() dto: CreateFcmTokenDto): Promise<void> {
+    await this.fcmTokenService.saveFcmToken(dto.fcmToken, dto.deviceIdx);
+  }
+
+  @Post('/check-nickname')
+  @HttpCode(200)
+  @ApiOperation({ summary: '닉네임 중복검사' })
+  @Exception(400, '유효하지 않은 요청')
+  @Exception(409, '중복된 닉네임')
+  @ApiResponse({
+    status: 200,
+    description: '사용가능한 닉네임일경우 상태코드 200반환',
+  })
+  async checkNicknameDuplicate(
+    @Body() dto: CheckNicknameDuplicateDto,
+  ): Promise<void> {
+    const duplicatedUser = await this.userService.getUser({
+      nickname: dto.nickname,
+    });
+
+    if (duplicatedUser && duplicatedUser.nickname == dto.nickname) {
+      throw new ConflictException('Duplicated Nickname');
+    }
+
+    if (dto.nickname && dto.nickname.includes('번째 오리')) {
+      throw new BadRequestException("Nickname can't include '번째 오리'");
+    }
+  }
+
+  @Post('/signup')
+  @ApiOperation({ summary: '회원가입' })
+  @Exception(400, '유효하지 않은 요청')
+  @Exception(409, '유효하지않은 닉네임/이메일이거나 이미가입된 회원입니다')
+  @ApiResponse({ status: 201, type: UserEntity })
+  async signUp(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
+    return await this.userService.createUser(createUserDto);
+  }
+
+  @Post('pw/reset')
+  @ApiOperation({
+    summary: '비밀번호 변경',
+    description: '비밀번호 변경 성공시 상태코드 200반환',
+  })
+  @HttpCode(200)
+  @Exception(400, '유효하지 않은 요청')
+  @Exception(401, '권한 없음')
+  @ApiResponse({ status: 200 })
+  async resetPassword(@Body() dto: ResetPasswordDto): Promise<void> {
+    await this.userService.updatePassword(dto.email, dto.pw);
+  }
+
+  @Get('myinfo')
+  @UseGuards(AuthGuard)
+  @ApiOperation({ summary: '내정보보기' })
+  @ApiBearerAuth()
+  @Exception(401, '권한 없음')
+  @ApiResponse({ status: 200, type: UserEntity })
+  async GetMyInfo(@GetUser() loginUser: LoginUser): Promise<UserEntity> {
+    return await this.userService.getUser({ idx: loginUser.idx });
+  }
+
+  @Put('profile-img')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @UseInterceptors(FileInterceptor('image'))
+  @ApiOperation({ summary: '프로필 이미지 수정' })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    required: true,
+    description: '프로필 이미지',
+    schema: {
+      type: 'object',
+      properties: {
+        imgPath: {
+          type: 'string',
+          format: 'binary',
+          description: '업로드할 이미지 파일',
+        },
+      },
+    },
+  })
+  @Exception(400, '유효하지 않은 요청')
+  @Exception(401, '권한 없음')
+  @ApiResponse({ status: 200 })
+  async updateMyProfileImg(
+    @GetUser() loginUser: LoginUser,
+    @UploadedFile(FileValidationPipe) image: Express.Multer.File,
+  ): Promise<{ imgPath: string }> {
+    const imgPath = await this.awsService.uploadImageToS3(image);
+
+    await this.userService.updateMyProfileImg(loginUser.idx, imgPath, null);
+
+    return { imgPath: imgPath };
+  }
+
+  @Delete('profile-img')
+  @UseGuards(AuthGuard)
+  @ApiOperation({ summary: '프로필 이미지 삭제' })
+  @ApiBearerAuth()
+  @Exception(401, '권한 없음')
+  @ApiResponse({ status: 200 })
+  async deleteMyProfileImg(@GetUser() loginUser: LoginUser): Promise<void> {
+    await this.userService.updateMyProfileImg(loginUser.idx, null, null);
+  }
+
+  @Put('myinfo')
+  @UseGuards(AuthGuard)
+  @ApiOperation({
+    summary: '내정보수정',
+    description: '최소 1개이상의 속성을 주셔야합니다. ',
+  })
+  @ApiBearerAuth()
+  @Exception(400, '유효하지 않은 요청')
+  @Exception(401, '권한 없음')
+  @ApiResponse({ status: 200 })
+  async updateMyInfo(
+    @GetUser() loginUser: LoginUser,
+    @Body() dto: UpdateMyInfoDto,
+  ): Promise<UserEntity> {
+    return await this.userService.updateMyinfo(loginUser.idx, dto);
+  }
+
+  @Get('')
+  @UseGuards(OptionalAuthGuard)
+  @ApiOperation({ summary: '유저검색하기 이메일, 닉네임, 관심사' })
+  @ApiQuery({ name: 'search', description: '검색 키워드, 검색어 2글자 이상' })
+  @Exception(400, '유효하지 않은 요청')
+  @Exception(404, 'Not Found Page')
+  @ApiResponse({ status: 200, type: UserPagerbleResponseDto })
+  async getUsersWithSearch(
+    @GetUser() loginUser: LoginUser,
+    @Query() dto: GetUserSearchDto,
+  ): Promise<UserPagerbleResponseDto> {
+    return await this.userService.getSearchedUsersWithInteraction(
+      dto,
+      loginUser,
+    );
+  }
+
+  @Delete('')
+  @UseGuards(AuthGuard)
+  @ApiOperation({ summary: '유저 탈퇴하기' })
+  @ApiBearerAuth()
+  @Exception(401, '권한 없음')
+  @ApiResponse({ status: 200 })
+  async deleteUser(@GetUser() loginUser: LoginUser): Promise<void> {
+    await this.userService.deleteUser(loginUser.idx);
   }
 }

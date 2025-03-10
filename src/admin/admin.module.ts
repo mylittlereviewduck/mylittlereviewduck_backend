8a7cd6c45d653f1085ba9d33a1c01b87a23e15b3
k1ju <gksrlwn98@naver.com>
2025-03-10 22:22:14 +0900

    Fix: add AdminModule, UserModule, ReportModule into AdminModule

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,11 +3,14 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { OpinionModule } from 'src/opinion/opinion.module';
+import { UserSuspensionService } from './user-suspension.service';
+import { UserModule } from 'src/user/user.module';
+import { ReportModule } from 'src/report/report.module';
 
 @Module({
-  imports: [PrismaModule, OpinionModule],
+  imports: [PrismaModule, OpinionModule, UserModule, ReportModule],
   controllers: [AdminController],
-  providers: [AdminService],
+  providers: [AdminService, UserSuspensionService],
   exports: [],
 })
 export class AdminModule {}

6b73d5ea2d5a1a4c0285735ca6d71e379902853b
k1ju <gksrlwn98@naver.com>
2025-03-10 17:07:33 +0900

    Feat: add OpinionEntity

--- src/user/entity/Opinion.entity.ts
+++ src/user/entity/Opinion.entity.ts
@@ -1,14 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { ReviewUserEntity } from 'src/review/entity/ReviewUser.entity';
 
 export class OpinionEntity {
   @ApiProperty({ example: '1', description: '의견엔티티 식별자' })
   idx: number;
 
   @ApiProperty({
-    example: 'de1704a4-bdd4-4df5-8fe8-053338cbac44',
-    description: '작성자 식별자',
+    example: {
+      idx: '344e753e-9071-47b2-b651-bc32a0a92b1f',
+      email: 'test1@a.com',
+      nickname: '23번째 오리',
+      profileImg:
+        'https://s3.ap-northeast-2.amazonaws.com/todayreview/1724893124840.png',
+      interest1: '여행',
+      interest2: null,
+    },
+    description: '작성자',
   })
-  accountIdx: string;
+  user: ReviewUserEntity;
 
   @ApiProperty({ example: '의견 제목입니다.', description: '의견 제목' })
   title: string;
@@ -36,7 +45,7 @@ export class OpinionEntity {
 
   constructor(data) {
     this.idx = data.idx;
-    this.accountIdx = data.accountIdx;
+    this.user = new ReviewUserEntity(data.accountTb);
     this.title = data.title;
     this.content = data.content;
     this.status = data.status;

6dcf8da9057ad32c03d6f524a4883f591ef3e259
k1ju <gksrlwn98@naver.com>
2025-03-10 18:27:02 +0900

    Fix: add OpinionController

--- src/opinion/opinion.module.ts
+++ src/opinion/opinion.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { OpinionService } from './opinion.service';
+import { OpinionController } from './opinion.controller';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [],
+  controllers: [OpinionController],
   providers: [OpinionService],
   exports: [],
 })

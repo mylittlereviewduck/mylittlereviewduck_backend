8b7c17899e2bb9b5f03b8a418a0fe42c1ed9c43a
k1ju <gksrlwn98@naver.com>
2025-03-10 19:18:10 +0900

    Feat: add get /user/my-opinion

--- src/opinion/opinion.controller.ts
+++ src/opinion/opinion.controller.ts
@@ -1,10 +1,13 @@
+import { OpinionPagerbleResponseDto } from './dto/response/opinion-pagerble.response.dto';
 import {
   Body,
   Controller,
+  Get,
   Param,
   ParseIntPipe,
   Post,
   Put,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import {
@@ -21,13 +24,15 @@ import { OpinionEntity } from './entity/Opinion.entity';
 import { LoginUser } from 'src/auth/model/login-user.model';
 import { GetUser } from 'src/common/decorator/get-user.decorator';
 import { UpsertOpinionDto } from './dto/upsert-opinion.dto';
+import { PagerbleDto } from 'src/user/dto/pagerble.dto';
+import { AdminGuard } from 'src/auth/guard/admin.guard';
 
-@Controller('user')
+@Controller('')
 @ApiTags('opinion')
 export class OpinionController {
   constructor(private readonly opinionService: OpinionService) {}
 
-  @Post('/opinion')
+  @Post('/user/opinion')
   @UseGuards(AuthGuard)
   @ApiBearerAuth()
   @ApiOperation({
@@ -48,7 +53,32 @@ export class OpinionController {
     return await this.opinionService.createOpinion(loginUser.idx, dto);
   }
 
-  @Put('/opinion/:opinionIdx')
+  @Get('/user/my-opinion')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: '내가 남긴 의견 목록 가져오기',
+    description: '내가 남긴 의견 목록 가져오기(페이지네이션)',
+  })
+  @Exception(400, '유효하지 않은 요청')
+  @Exception(401, '권한 없음')
+  @ApiResponse({
+    status: 200,
+    description: '성공시 200 반환',
+    type: OpinionPagerbleResponseDto,
+  })
+  async getMyOpinions(
+    @GetUser() loginUser: LoginUser,
+    @Query() dto: PagerbleDto,
+  ): Promise<OpinionPagerbleResponseDto> {
+    return await this.opinionService.getOpinions({
+      userIdx: loginUser.idx,
+      page: dto.page,
+      size: dto.size,
+    });
+  }
+
+  @Put('/user/opinion/:opinionIdx')
   @UseGuards(AuthGuard)
   @ApiBearerAuth()
   @ApiOperation({

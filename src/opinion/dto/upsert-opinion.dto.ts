ee2ad1511ec1b2bc90866d57247aeb8647d158c9
k1ju <gksrlwn98@naver.com>
2025-03-10 18:26:02 +0900

    Docs: add swagger upsertOpinionDto

--- src/opinion/dto/upsert-opinion.dto.ts
+++ src/opinion/dto/upsert-opinion.dto.ts
@@ -1,10 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, Length } from 'class-validator';
 
 export class UpsertOpinionDto {
+  @ApiProperty({
+    example: '의견 제목입니다',
+    description: '의견 제목',
+    required: true,
+  })
   @IsString()
   @Length(1, 150)
   title: string;
 
+  @ApiProperty({
+    example: '의견 내용입니다',
+    description: '의견 내용',
+    required: true,
+  })
   @IsString()
   @Length(1, 5000)
   content: string;

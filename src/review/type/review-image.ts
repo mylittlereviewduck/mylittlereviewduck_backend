66732c5815fab726712195c28b52240a7e4ad4ba
k1ju <gksrlwn98@naver.com>
2025-03-17 00:34:26 +0900

    Debug: add validation create review

--- src/review/type/review-image.ts
+++ src/review/type/review-image.ts
@@ -1,5 +1,10 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class ReviewImage {
+  @IsString()
+  @IsNotEmpty()
   image: string;
 
+  @IsOptional()
   content: string | null;
 }

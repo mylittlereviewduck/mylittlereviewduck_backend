526a0d1cb4122dd972dda960e6351baf6b461735
k1ju <gksrlwn98@naver.com>
2025-04-29 12:45:39 +0900

    Feat: add adminService createAnnouncementDto

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,7 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateAnnouncementDto } from './dto/create-announcement.dto';
+import { AnnouncementEntity } from './entity/Announcement.entity';
 
 @Injectable()
 export class AdminService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async createAnnouncement(
+    dto: CreateAnnouncementDto,
+    userIdx: string,
+  ): Promise<AnnouncementEntity> {
+    const announcementData = await this.prismaService.announcementTb.create({
+      data: {
+        accountIdx: userIdx,
+        title: dto.title,
+        content: dto.content,
+        category: dto.category,
+        status: dto.status,
+      },
+      include: {
+        accountTb: true,
+      },
+    });
+
+    return new AnnouncementEntity(announcementData);
+  }
 }
